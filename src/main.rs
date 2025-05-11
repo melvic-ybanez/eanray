f63647f72ed1e2b254752852bc2e0f12e19c5408
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2025-05-12 01:57:11 +0800

    Read the input from the stdin

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,14 @@
 use std::io;
+use std::io::Read;
 use crate::dsl::ast::Scene;
 
 mod dsl;
 mod core;
 
 fn main() -> io::Result<()> {
-    let raw_scene = r#"{
-        "camera": {"aspect_ratio": [16, 9], "image_width": 400},
-        "objects": [
-            {"sphere": { "center": [0, 0, -1], "radius": 0.5 }},
-            {"sphere": { "center": [0, -100.5, -1], "radius": 100.0 }}
-        ]
-    }"#;
-    let scene: Scene = serde_json::from_str(raw_scene)?;
+    let mut raw_scene = String::new();
+    io::stdin().read_to_string(&mut raw_scene)?;
+    let scene: Scene = serde_json::from_str(&raw_scene)?;
     let (camera, world) = scene.build();
     camera.render(world)
 }

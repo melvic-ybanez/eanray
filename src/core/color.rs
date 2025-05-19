ba15c8d3da3345e9d584edf156845fb75639e6b5
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2025-05-20 02:00:08 +0800

    Gamma correction

--- src/core/color.rs
+++ src/core/color.rs
@@ -1,10 +1,6 @@
-use std::fmt;
-use std::fmt::Formatter;
-use crate::core::math::Real;
 use crate::core::math::interval::Interval;
 use crate::core::math::vector::{CanAdd, Vec3D, VecLike};
-use std::fs::File;
-use std::io::{self, Write};
+use crate::core::math::Real;
 
 pub struct ColorKind;
 
@@ -34,14 +30,29 @@ impl Color {
     pub fn red() -> Color {
         Color::new(1.0, 0.0, 0.0)
     }
-    
+
     pub fn to_bytes_string(&self) -> String {
         let intensity = Interval::new(0.0, 0.999);
-        let r_byte = (intensity.clamp(self.red_component()) * 256.0) as u16;
-        let g_byte = (intensity.clamp(self.green_component()) * 256.0) as u16;
-        let b_byte = (intensity.clamp(self.blue_component()) * 256.0) as u16;
 
-        format!("{} {} {}", r_byte, g_byte, b_byte)
+        let to_byte = |component: Real| -> u16 {
+            let component = Color::linear_to_gamma(component);
+            (intensity.clamp(component) * 256.0) as u16
+        };
+
+        format!(
+            "{} {} {}",
+            to_byte(self.red_component()),
+            to_byte(self.green_component()),
+            to_byte(self.blue_component())
+        )
+    }
+
+    fn linear_to_gamma(linear_component: Real) -> Real {
+        if linear_component > 0.0 {
+            linear_component.sqrt()
+        } else {
+            0.0
+        }
     }
 }
 

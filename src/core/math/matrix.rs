0925bddbbd4b00dc2aceba78a126bea24b5ebe6c
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2025-09-03 03:26:53 +0800

    Generalize matrix

--- src/core/math/matrix.rs
+++ src/core/math/matrix.rs
@@ -4,45 +4,36 @@ use crate::core::math::Real;
 use std::default::Default;
 use std::ops::{Index, IndexMut, Mul};
 
-type Table4x4 = [Real; 16];
-type Table3x3 = [Real; 9];
-type Table2x2 = [Real; 4];
-
-/// Represents a 4x4 matrix, as the name suggests.
-/// Note that there is no need to generalize the matrix to have arbitrary size because
-/// most of the operations we care about here only involve 4x4 matrices.
 #[derive(Clone, Debug)]
-pub(crate) struct Matrix4x4 {
-    elems: Table4x4,
+pub(crate) struct Matrix {
+    elems: Vec<Real>,
+    order: usize,
+    determinant: Real,
 }
 
-impl Matrix4x4 {
-    //noinspection RsConstNaming
-    #[rustfmt::skip]
-    pub(crate) const IDENTITY_4x4: Matrix4x4 = Self {
-        elems: [
-            1.0, 0.0, 0.0, 0.0,
-            0.0, 1.0, 0.0, 0.0,
-            0.0, 0.0, 1.0, 0.0,
-            0.0, 0.0, 0.0, 1.0,
-        ],
-    };
-
-    pub(crate) fn from_array(elems: Table4x4) -> Self {
-        Self { elems }
+impl Matrix {
+    pub(crate) fn from_vec(elems: Vec<Real>) -> Self {
+        let order = elems.len().isqrt();
+        let mut this = Self {
+            elems,
+            order,
+            determinant: 0.0,
+        };
+        this.compute_determinant();
+        this
     }
 
-    pub(crate) fn from_2df(elems: [[Real; 4]; 4]) -> Self {
-        Self::from_array(elems.concat().try_into().unwrap())
+    pub(crate) fn fill(order: usize, value: Real) -> Self {
+        Self::from_vec(vec![value; order * order])
     }
 
-    pub(crate) fn from_2di(elems: [[u32; 4]; 4]) -> Self {
-        Self::from_2df(elems.map(|row| row.map(|f| f as Real)))
+    pub(crate) fn fill_default(order: usize) -> Self {
+        Self::fill(order, Real::default())
     }
 
     pub(crate) fn transpose(&self) -> Self {
-        let mut transposition: Matrix4x4 = self.clone();
-        matrix_loop(|row, col| {
+        let mut transposition: Matrix = self.clone();
+        self.traverse(|row, col| {
             transposition[(row, col)] = self[(col, row)];
         });
         transposition
@@ -50,50 +41,52 @@ impl Matrix4x4 {
 
     /// Yields a new smaller matrix that does not contain the
     /// row `row` and column `col`.
-    fn submatrix(&self, row: usize, col: usize) -> Table3x3 {
-        let mut table = Table3x3::default();
-        let mut i = 0;
-        matrix_loop(|r, c| {
+    fn submatrix(&self, row: usize, col: usize) -> Matrix {
+        let mut table = vec![];
+
+        self.traverse(|r, c| {
             if r != row && c != col {
-                table[i] = self[(r, c)];
-                i += 1;
+                table.push(self[(r, c)]);
             }
         });
-        table
+
+        Self::from_vec(table)
     }
 
-    fn determinant(&self) -> Real {
-        let mut det = 0.0;
-        for col in 0..4 {
-            det += self[(0, col)] * self.cofactor(0, col);
+    fn compute_determinant(&mut self) {
+        if self.order == 2 {
+            self.determinant = self[(0, 0)] * self[(1, 1)] - self[(0, 1)] * self[(1, 0)]
+        } else {
+            for col in 0..self.order {
+                self.determinant += self[(0, col)] * self.cofactor(0, col);
+            }
         }
-        det
+    }
+
+    /// The minor at (`row`, `col`) of a matrix, defined as
+    /// the determinant of the submatrix at (`row`, `col`).
+    fn minor(&self, row: usize, col: usize) -> Real {
+        self.submatrix(row, col).determinant
     }
 
     fn cofactor(&self, row: usize, col: usize) -> Real {
-        cofactor_from_minor(self.minor(row, col), row, col)
-    }
-
-    /// The minor at (`row`, `col`) of a 4x4 matrix, defined as
-    /// the determinant of the submatrix at (`row`, `col`).
-    fn minor(&self, row: usize, col: usize) -> Real {
-        matrix_3x3::determinant(self.submatrix(row, col))
+        let minor = self.minor(row, col);
+        if (row + col) % 2 == 0 { minor } else { -minor }
     }
 
     fn is_invertible(&self) -> bool {
-        self.determinant() != 0.0
+        self.determinant != 0.0
     }
 
-    fn inverse(&self) -> Option<Matrix4x4> {
+    fn inverse(&self) -> Option<Matrix> {
         if self.is_invertible() {
-            let mut matrix = Matrix4x4::default();
-            let determinant = self.determinant();
+            let mut matrix = Self::fill_default(self.order);
 
-            matrix_loop(|row, col| {
+            self.traverse(|row, col| {
                 let c = self.cofactor(row, col);
 
                 // we are reversing `row` and `col` to transpose the matrix
-                matrix[(col, row)] = c / determinant;
+                matrix[(col, row)] = c / self.determinant;
             });
 
             Some(matrix)
@@ -101,54 +94,51 @@ impl Matrix4x4 {
             None
         }
     }
-}
-mod matrix_3x3 {
-    use super::*;
 
-    pub(super) fn determinant(matrix: Table3x3) -> Real {
-        let mut det = 0.0;
-        for col in 0..3 {
-            det += matrix[fold_index((0, col), 3)] * cofactor(matrix, 0, col);
-        }
-        det
-    }
-
-    fn submatrix(matrix_3x3: Table3x3, row: usize, col: usize) -> Table2x2 {
-        let mut table = Table2x2::default();
-        let mut i = 0;
-
-        for index in 0..9 {
-            let (r, c) = unfold_index(index, 3);
-            if r != row && c != col {
-                table[i] = matrix_3x3[index];
-                i += 1;
+    fn traverse<F>(&self, mut f: F)
+    where
+        F: FnMut(usize, usize) -> (),
+    {
+        for row in 0..self.order {
+            for col in 0..self.order {
+                f(row, col);
             }
         }
-
-        table
-    }
-
-    fn minor(matrix_3x3: Table3x3, row: usize, col: usize) -> Real {
-        determinant_2x2(submatrix(matrix_3x3, row, col))
-    }
-
-    fn cofactor(matrix_3x3: Table3x3, row: usize, col: usize) -> Real {
-        let minor = minor(matrix_3x3, row, col);
-        cofactor_from_minor(minor, row, col)
     }
 }
 
-fn determinant_2x2(matrix_2x2: Table2x2) -> Real {
-    let a = matrix_2x2[fold_index((0, 0), 2)];
-    let b = matrix_2x2[fold_index((0, 1), 2)];
-    let c = matrix_2x2[fold_index((1, 0), 2)];
-    let d = matrix_2x2[fold_index((1, 1), 2)];
+mod matrix_4x4 {
+    use super::*;
 
-    a * d - b * c
-}
+    #[rustfmt::skip]
+    pub(crate) fn identity() -> Matrix {
+        from_array([
+            1.0, 0.0, 0.0, 0.0,
+            0.0, 1.0, 0.0, 0.0,
+            0.0, 0.0, 1.0, 0.0,
+            0.0, 0.0, 0.0, 1.0,
+        ])
+    }
 
-fn cofactor_from_minor(minor: Real, row: usize, col: usize) -> Real {
-    if (row + col) % 2 == 0 { minor } else { -minor }
+    pub(crate) fn fill(value: Real) -> Matrix {
+        Matrix::fill(4, value)
+    }
+
+    pub(crate) fn fill_default() -> Matrix {
+        Matrix::fill_default(4)
+    }
+
+    pub(crate) fn from_array(elems: [Real; 16]) -> Matrix {
+        Matrix::from_vec(elems.to_vec())
+    }
+
+    pub(crate) fn from_2df(elems: [[Real; 4]; 4]) -> Matrix {
+        from_array(elems.concat().try_into().unwrap())
+    }
+
+    pub(crate) fn from_2di(elems: [[u32; 4]; 4]) -> Matrix {
+        from_2df(elems.map(|row| row.map(|f| f as Real)))
+    }
 }
 
 fn fold_index(index: (usize, usize), size: usize) -> usize {
@@ -156,53 +146,44 @@ fn fold_index(index: (usize, usize), size: usize) -> usize {
     row * size + col
 }
 
-fn unfold_index(index: usize, size: usize) -> (usize, usize) {
-    (index / size, index % size)
-}
-
-impl Default for Matrix4x4 {
-    fn default() -> Self {
-        Self {
-            elems: Table4x4::default(),
-        }
-    }
-}
-
-impl PartialEq for Matrix4x4 {
+impl PartialEq for Matrix {
     fn eq(&self, other: &Self) -> bool {
-        for i in 0..16 {
-            if (self[i] - other[i]).abs() >= 1e-5 {
-                return false;
+        self.order == other.order && {
+            for i in 0..self.order * self.order {
+                if (self[i] - other[i]).abs() >= 1e-5 {
+                    return false;
+                }
             }
+            true
         }
-        true
     }
 }
 
-impl_index!(usize, Matrix4x4, Real, elems);
+impl_index!(usize, Matrix, Real, elems);
 
-impl Index<(usize, usize)> for Matrix4x4 {
+impl Index<(usize, usize)> for Matrix {
     type Output = Real;
 
     fn index(&self, index: (usize, usize)) -> &Self::Output {
-        &self[fold_index(index, 4)]
+        &self[fold_index(index, self.order)]
     }
 }
 
-impl IndexMut<(usize, usize)> for Matrix4x4 {
+impl IndexMut<(usize, usize)> for Matrix {
     fn index_mut(&mut self, index: (usize, usize)) -> &mut Self::Output {
-        &mut self[fold_index(index, 4)]
+        let order = self.order;
+        &mut self[fold_index(index, order)]
     }
 }
 
-impl Mul<&Matrix4x4> for &Matrix4x4 {
-    type Output = Matrix4x4;
+impl Mul<&Matrix> for &Matrix {
+    type Output = Matrix;
 
-    fn mul(self, other: &Matrix4x4) -> Self::Output {
-        let mut result: Matrix4x4 = Matrix4x4::default();
-        matrix_loop(|row, col| {
+    fn mul(self, other: &Matrix) -> Self::Output {
+        let mut result: Matrix = Matrix::fill_default(other.order);
+        other.traverse(|row, col| {
             // this is effectively a series of dot products
-            for i in 0..4 {
+            for i in 0..other.order {
                 result[(row, col)] += self[(row, i)] * other[(i, col)];
             }
         });
@@ -210,29 +191,18 @@ impl Mul<&Matrix4x4> for &Matrix4x4 {
     }
 }
 
-impl Mul<&Tuple4> for &Matrix4x4 {
+impl Mul<&Tuple4> for &Matrix {
     type Output = Tuple4;
 
     fn mul(self, tuple: &Tuple4) -> Self::Output {
         let mut result = Elems::default();
-        for row in 0..4 {
+        for row in 0..self.order {
             // like dot product, but for a single column
-            for i in 0..4 {
+            for i in 0..self.order {
                 result[row] += self[(row, i)] * tuple[i];
             }
         }
         Tuple4::from_elems(result)
-    }
-}
-
-fn matrix_loop<F>(mut f: F)
-where
-    F: FnMut(usize, usize) -> (),
-{
-    for row in 0..4 {
-        for col in 0..4 {
-            f(row, col);
-        }
     }
 }
 
@@ -243,69 +213,69 @@ mod tests {
     #[rustfmt::skip]
     #[test]
     fn test_submatrices() {
-        let matrix = Matrix4x4::from_2di([
+        let matrix = matrix_4x4::from_2di([
             [1, 2, 3, 4],
             [0, 5, 6, 7],
             [8, 9, 1, 2],
             [3, 4, 5, 6]
         ]);
 
-        let submatrix = [
-            [1, 2, 4],
-            [8, 9, 2],
-            [3, 4, 6]
-        ];
+        let submatrix = Matrix::from_vec(vec![
+            1.0, 2.0, 4.0,
+            8.0, 9.0, 2.0,
+            3.0, 4.0, 6.0
+        ]);
 
-        assert_eq!(matrix.submatrix(1, 2), to_table_3x3_f(submatrix));
+        assert_eq!(matrix.submatrix(1, 2), submatrix);
 
-        let matrix = Matrix4x4::from_2di([
+        let matrix = matrix_4x4::from_2di([
             [4, 1, 0, 2],
             [7, 5, 9, 3],
             [6, 8, 2, 1],
             [5, 0, 3, 4]
         ]);
 
-        let submatrix = [
-            [5, 9, 3],
-            [8, 2, 1],
-            [0, 3, 4]
-        ];
+        let submatrix = Matrix::from_vec(vec![
+            5.0, 9.0, 3.0,
+            8.0, 2.0, 1.0,
+            0.0, 3.0, 4.0
+        ]);
 
-        assert_eq!(matrix.submatrix(0, 0), to_table_3x3_f(submatrix));
+        assert_eq!(matrix.submatrix(0, 0), submatrix);
     }
 
     #[rustfmt::skip]
     #[test]
     fn test_determinants() {
-        let matrix = Matrix4x4::from_2di([
+        let matrix = matrix_4x4::from_2di([
             [1, 2, 3, 4],
             [2, 0, 1, 5],
             [3, 1, 2, 6],
             [4, 2, 1, 0]
         ]);
-        assert_eq!(matrix.determinant(), -3.0);
+        assert_eq!(matrix.determinant, -3.0);
 
-        let matrix = Matrix4x4::from_2di([
+        let matrix = matrix_4x4::from_2di([
             [2, 1, 0, 3],
             [4, 5, 6, 1],
             [7, 0, 8, 2],
             [1, 2, 3, 4]
         ]);
-        assert_eq!(matrix.determinant(), 450.0);
+        assert_eq!(matrix.determinant, 450.0);
 
-        let matrix = Matrix4x4::from_2di([
+        let matrix = matrix_4x4::from_2di([
             [0, 2, 1, 3],
             [4, 1, 5, 2],
             [7, 0, 6, 1],
             [3, 2, 4, 0]
         ]);
-        assert_eq!(matrix.determinant(), -51.0);
+        assert_eq!(matrix.determinant, -51.0);
     }
 
     #[rustfmt::skip]
     #[test]
     fn test_minors_and_cofactors() {
-        let matrix = Matrix4x4::from_2di([
+        let matrix = matrix_4x4::from_2di([
             [1, 2, 3, 4],
             [0, 5, 6, 7],
             [8, 9, 1, 2],
@@ -314,7 +284,7 @@ mod tests {
         assert_eq!(matrix.minor(0, 0), -9.0);
         assert_eq!(matrix.cofactor(0, 0), -9.0);
 
-        let matrix = Matrix4x4::from_2di([
+        let matrix = matrix_4x4::from_2di([
             [2, 3, 4, 1],
             [0, 5, 6, 7],
             [8, 1, 2, 3],
@@ -328,7 +298,7 @@ mod tests {
     #[test]
     fn test_invertibility() {
         assert!(
-            !Matrix4x4::from_2di([
+            !matrix_4x4::from_2di([
                 [1, 2, 3, 4],
                 [2, 4, 6, 8],
                 [1, 0, 1, 0],
@@ -336,7 +306,7 @@ mod tests {
             ]).is_invertible(),
         );
         assert!(
-            Matrix4x4::from_2di([
+            matrix_4x4::from_2di([
                 [2, 0, 1, 3],
                 [1, 2, 0, 4],
                 [0, 5, 1, 2],
@@ -344,7 +314,7 @@ mod tests {
             ]).is_invertible()
         );
         assert!(
-            !Matrix4x4::from_2di([
+            !matrix_4x4::from_2di([
                 [1, 0, 0, 0],
                 [0, 2, 0, 0],
                 [0, 0, 3, 0],
@@ -357,13 +327,13 @@ mod tests {
     #[test]
     fn test_inverses() {
         assert_eq!(
-            Matrix4x4::from_2di([
+            matrix_4x4::from_2di([
                 [1, 2, 0, 3],
                 [0, 1, 4, 2],
                 [5, 0, 1, 1],
                 [2, 3, 0, 1]
             ]).inverse(),
-            Some(Matrix4x4::from_2df([
+            Some(matrix_4x4::from_2df([
                 [-1.0 / 21.0, -1.0 / 21.0, 4.0 / 21.0, 1.0 / 21.0],
                 [-16.0 / 147.0, 5.0 / 147.0, -20.0 / 147.0, 58.0 / 147.0],
                 [-9.0 / 49.0, 12.0 / 49.0, 1.0 / 49.0, 2.0 / 49.0],
@@ -371,26 +341,18 @@ mod tests {
             ]))
         );
         assert_eq!(
-            Matrix4x4::from_2di([
+            matrix_4x4::from_2di([
                 [3, 0, 2, 1],
                 [1, 2, 0, 1],
                 [0, 1, 1, 0],
                 [2, 3, 0, 1]
             ]).inverse(),
-            Some(Matrix4x4::from_2df([
+            Some(matrix_4x4::from_2df([
                 [0.16667, -0.83333, -0.33333, 0.66667],
                 [-0.16667, -0.16667, 0.33333, 0.33333],
                 [0.16667, 0.16667, 0.66667, -0.33333],
                 [0.16667, 2.16667, -0.33333, -1.33333]
             ]))
         )
-    }
-
-    fn to_table_3x3_f(table: [[u32; 3]; 3]) -> Table3x3 {
-        table
-            .map(|row| row.map(|f| f as Real))
-            .concat()
-            .try_into()
-            .unwrap()
     }
 }
